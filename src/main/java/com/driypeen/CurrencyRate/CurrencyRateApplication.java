850de5777522fb523595437a32db8f7b6e616bde
driypeen <driypeen@gmail.com>
2022-06-02 13:35:43 +0400

    Enable feign

--- src/main/java/com/driypeen/CurrencyRate/CurrencyRateApplication.java
+++ src/main/java/com/driypeen/CurrencyRate/CurrencyRateApplication.java
@@ -2,8 +2,10 @@ package com.driypeen.CurrencyRate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CurrencyRateApplication {
 
 	public static void main(String[] args) {
